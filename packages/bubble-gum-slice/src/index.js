522e1bfb5f221db10f0a54b0a2aa6ae4d4eb9558
Nicolas Quiceno <nquicenob@gmail.com>
2016-11-12 01:24:06 +0100

    beginning of the implementation of the test in bubble-gum-slice

---
+++ packages/bubble-gum-slice/src/index.js
@@ -0,0 +1,13 @@
+
+/**
+ * Slice the object generating a new object
+ *
+ * @param {(Object|*[])} target - object target
+ * @param {Object[]} config - Array with the configuration of the slice function
+ * @param {*[]} config[].path - Array path to the property to be sliced
+ * @param {*[]} config[].newPath - Array path to the property in the new object if it is undefined, it will have the same value as the config[].path
+ * @return {Object} - Object with new values
+ */
+export default function slice(target, config) {
+  // TODO: implement it
+};
--- packages/bubble-gum-slice/test/index.js
+++ packages/bubble-gum-slice/test/index.js
@@ -1,2 +1,46 @@
-console.log('bubble-gum-slice');
-console.log('====');
+const tape = require('tape');
+const bubbleGumSlice = require('../src');
+
+tape('bubble-gum-slice - api', t => {
+  t.plan(2);
+  t.is(typeof bubbleGumSlice, 'function', 'should be a function');
+  t.is(bubbleGumSlice.length, 2, 'should receive three params: "target", "config"');
+});
+
+tape('slice(target, config) - base', t => {
+  t.plan(2);
+  const testObj = {
+    a: 'a'
+    b: 'b'
+    deep: {
+      deep1: 1,
+      deep2: 2,
+    },
+  };
+  t.same(bubbleGumSlice(testObj, [{ path: ['a'] }]), { a: 'a' }, 'should create the new object');
+  t.same(bubbleGumSlice(testObj, [
+    { path: ['a'] },
+    { path: ['deep', 'deep1'] }
+  ]), {
+    a: 'a',
+    deep: { deep1: 1 },
+   }, 'should create the new object');
+
+   t.end();
+});
+
+tape('slice(target, config) - working with arrays', t => {
+
+});
+
+tape('slice(target, config) - rename path with "newPath" property in config param', t => {
+
+});
+
+tape('slice(target, config) - special cases', t => {
+
+});
+
+tape('slice(target, config) - throws a exception', t => {
+
+});
