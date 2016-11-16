684528febadde984aaeb50cdb376c697d8da92bb
Nicolas Quiceno <nquicenob@gmail.com>
2016-11-16 18:14:17 +0100

    Improvement in test script

--- scripts/test.js
+++ scripts/test.js
@@ -1,15 +1,36 @@
+require('babel-register');
+require('babel-polyfill');
+
+// npm run test <alias> - get set slice create has goto 
+
 const glob = require('glob');
-const requireAll = require('require-all');
 const minimist = require('minimist');
+const { alias, rootDir } = require('../package.json').scriptsConfig;
 
-const { _ : packages } = minimist(process.argv.slice(2));
-const PACKAGES_FOLDER= `${process.env.PROJECT_PATH}/packages`;
+const PACKAGES_FOLDER= process.env.PROJECT_PATH;
+const { _ : packagesAlias } = minimist(process.argv.slice(2));
 
-// TODO: ALIAS SUPORT
-function getTestDirs(packages, path) {
-  return (packages.length > 0) ?
-    packages.map(_package => `${path}/${_package}/test`)
-    : glob.sync(`${path}/**/test`);
+const getTestDirsByAlias = ({
+  packages,
+  path,
+  alias,
+}) => packages.map(aliasName => `${path}/${alias[aliasName]}/test/*.js`)
+  .reduce((testDirs, globPattern) => testDirs.concat(glob.sync(globPattern)), []);
+
+const getAllTestDirs = ({
+  path,
+  rootDir,
+}) => glob.sync(`${path}/${rootDir}/*/test/*.js`);
+
+function getTestDirs(packages) {
+  const fn = [getAllTestDirs];
+  return (fn[packages.length] || getTestDirsByAlias)({
+    path: PACKAGES_FOLDER,
+    packages,
+    rootDir,
+    alias,
+  });
 }
 
-getTestDirs(packages, PACKAGES_FOLDER).forEach(testDir => requireAll(testDir))
+const testDirs = getTestDirs(packagesAlias);
+testDirs.forEach(fileName => require(fileName));
