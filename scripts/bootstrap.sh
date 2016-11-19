88640a5b2e3b04f4155ed66a37479a0fb6fb3333
Nicolas Quiceno <nquicenob@gmail.com>
2016-11-19 18:16:07 +0100

    remove invalid files

--- scripts/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./node_modules/.bin/lerna bootstrap
-node ./scripts/_build.js
--- scripts/jia.sh
+++
@@ -1,2 +0,0 @@
-# ./node_modules/.bin/babel ./packages -d ./lib
-./node_modules/.bin/rollup ./packages/index.js --output ./lib/index.js
